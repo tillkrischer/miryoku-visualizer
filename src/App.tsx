c99626166e9091127b68c4642118fe29a0d74f7e
Till Krischer <till@krischer.io>
2023-06-18 18:04:15 +0200

    right half

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,63 @@
 // prettier-ignore
-const topPos = [
+const yPosLeft = [
   40, 27, 20, 27, 33,
   94, 81, 74, 81, 87,
   148, 135, 128, 135, 141,
   192, 202, 231
 ];
 // prettier-ignore
-const leftPos = [
+const yPosRight = [
+  33, 27, 20, 27, 40,
+  87, 81, 74, 81, 94,
+  141, 135, 128, 135, 148,
+  231, 202, 192
+]
+const yPos = [...yPosLeft, ...yPosRight]
+// prettier-ignore
+const xPosLeft = [
   20, 74, 128, 182, 236,
   20, 74, 128, 182, 236,
   20, 74, 128, 182, 236,
   150, 210, 263
 ];
 // prettier-ignore
-const rotation = [
+const xPosRight = [
+  380, 434, 488, 542, 596,
+  380, 434, 488, 542, 596,
+  380, 434, 488, 542, 596,
+  353, 406, 466
+]
+const xPos = [...xPosLeft, ...xPosRight]
+// prettier-ignore
+const rotationLeft = [
   0, 0, 0, 0, 0,
   0, 0, 0, 0, 0,
   0, 0, 0, 0, 0,
   0, 20, 40
 ];
 // prettier-ignore
-const alphas = [
+const rotationRight = [
+  0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 
+  0, 0, 0, 0, 0, 
+  -40, -20, 0
+]
+const rotation = [...rotationLeft, ...rotationRight]
+// prettier-ignore
+const alphasLeft = [
   "Q", "W", "E", "R", "T",
   "A", "S", "D", "F", "G",
   "Z", "X", "C", "V", "B",
   "Esc", "Spc", "⭾"
 ];
+// prettier-ignore
+const alphasRight = [
+  "Y", "U", "I", "O", "P",
+  "H", "J", "K", "L", "'",
+  "N", "M", ",", ".", "/",
+  "↵", "⌫", "⌦"
+];
+const alphas = [...alphasLeft, ...alphasRight]
 // prettier-ignore
 const symbols = [
   "{", "&", "*", "(", "}",
@@ -34,6 +66,14 @@ const symbols = [
   "(", ")", "_"
 ];
 // prettier-ignore
+const mouse = [
+  "Redo", "Paste", "Copy", "Cut", "Undo",
+  "", "🠘", "🠙", "🠛", "🠚",
+  "", "⇦", "⇧", "⇩", "⇨",
+  "R", "L", "M"
+];
+const layer2 = [...symbols, ...mouse]
+// prettier-ignore
 const functions = [
   "F12", "F7", "F8", "F9", "PrtScr",
   "F11", "F4", "F5", "F6", "ScrLo",
@@ -41,12 +81,31 @@ const functions = [
   "≡", "Spc", "⭾"
 ];
 // prettier-ignore
+const media = [
+  "R", "M", "H", "S", "V",
+  "E", "Prev", "VUp", "VDo", "Next",
+  "◯", "1", "2", "3", "4",
+  "Stop", "Play", "Mut"
+];
+const layer3 = [...functions, ...media]
+// prettier-ignore
 const nums = [
   "[", "7", "8", "9", "]",
   ";", "4", "5", "6", "=",
   "`", "1", "2", "3", "\\",
   ".", "0", "-"
 ];
+// prettier-ignore
+const navigation = [
+  "Redo", "Paste", "Copy", "Cut", "Undo",
+  "CapL", "⯇", "▲", "▼", "⯈",
+  "Ins", "Home", "PgD", "PgU", "End",
+  "↵", "⌫", "⌦"
+
+// ⯇ ⯈ ⯅ ⯆
+// ◀ ▶ ▲ ▼
+];
+const layer4 = [...nums, ...navigation]
 
 interface ButtonProps {
   side: 'left' | 'right'
@@ -79,27 +138,36 @@ const Button = (props: ButtonProps) => {
           leftBottom: 'text-[#fd4526]',
           rightBottom: 'text-[#4322fd]',
         }
-      : {}
+      : {
+          leftTop: 'text-[#000000]',
+          rightTop: 'text-[#ffff1b]',
+          leftBottom: 'text-[#ff4fff]',
+          rightBottom: 'text-[#4bffff]',
+        }
 
   return (
     <div
-      className='absolute h-[54px] w-[54px] rounded-[5px] border border-black bg-[#888888] text-xs font-bold'
+      className='absolute h-[54px] w-[54px] rounded-[5px] border border-black bg-[#888888] font-bold'
       style={{ top, left, transform: `rotate(${rotation}deg)` }}
     >
       <div className='absolute left-[5px] top-[3px] h-[42px] w-[42px] rounded-[3px] border border-[rgba(0,0,0,0.3)] bg-[#a7a7a7]'>
-        <div className={`absolute left-[1px] top-[1px] ${colors.leftTop}`}>
+        <div
+          className={`absolute left-[1px] top-[1px] text-[0.75rem] ${colors.leftTop}`}
+        >
           {leftTop}
         </div>
-        <div className={`absolute right-[1px] top-[1px] ${colors.rightTop}`}>
+        <div
+          className={`absolute right-[1px] top-[1px] text-[0.65rem] ${colors.rightTop}`}
+        >
           {rightTop}
         </div>
         <div
-          className={`absolute bottom-[1px] left-[1px] ${colors.leftBottom}`}
+          className={`absolute bottom-[1px] left-[1px] text-[0.65rem] ${colors.leftBottom}`}
         >
           {leftBottom}
         </div>
         <div
-          className={`absolute bottom-[1px] right-[1px] ${colors.rightBottom}`}
+          className={`absolute bottom-[1px] right-[1px] text-[0.65rem] ${colors.rightBottom}`}
         >
           {rightBottom}
         </div>
@@ -109,7 +177,7 @@ const Button = (props: ButtonProps) => {
 }
 
 const App = () => {
-  const indicies = [...Array(18).keys()]
+  const indicies = [...Array(36).keys()]
 
   return (
     <div className='App'>
@@ -119,14 +187,14 @@ const App = () => {
         {indicies.map((i) => (
           <Button
             key={i}
-            top={topPos[i]}
-            left={leftPos[i]}
+            top={yPos[i]}
+            left={xPos[i]}
             rotation={rotation[i]}
             side={i < 18 ? 'left' : 'right'}
             leftTop={alphas[i]}
-            rightTop={symbols[i]}
-            leftBottom={functions[i]}
-            rightBottom={nums[i]}
+            rightTop={layer2[i]}
+            leftBottom={layer3[i]}
+            rightBottom={layer4[i]}
           />
         ))}
       </div>
