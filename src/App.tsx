158b831e54f47f8071944a774ef7b14c33824e86
Till Krischer <till.krischer@materna.de>
2023-06-30 19:45:14 +0200

    title + layout

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,8 @@ const yPosRight = [
   87, 81, 74, 81, 94,
   141, 135, 128, 135, 148,
   231, 202, 192
-]
-const yPos = [...yPosLeft, ...yPosRight]
+];
+const yPos = [...yPosLeft, ...yPosRight];
 // prettier-ignore
 const xPosLeft = [
   20, 74, 128, 182, 236,
@@ -26,8 +26,8 @@ const xPosRight = [
   380, 434, 488, 542, 596,
   380, 434, 488, 542, 596,
   353, 406, 466
-]
-const xPos = [...xPosLeft, ...xPosRight]
+];
+const xPos = [...xPosLeft, ...xPosRight];
 // prettier-ignore
 const rotationLeft = [
   0, 0, 0, 0, 0,
@@ -38,11 +38,11 @@ const rotationLeft = [
 // prettier-ignore
 const rotationRight = [
   0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 
-  0, 0, 0, 0, 0, 
+  0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0,
   -40, -20, 0
-]
-const rotation = [...rotationLeft, ...rotationRight]
+];
+const rotation = [...rotationLeft, ...rotationRight];
 // prettier-ignore
 const alphasLeft = [
   "Q", "W", "E", "R", "T",
@@ -57,7 +57,7 @@ const alphasRight = [
   "N", "M", ",", ".", "/",
   "↵", "⌫", "⌦"
 ];
-const alphas = [...alphasLeft, ...alphasRight]
+const alphas = [...alphasLeft, ...alphasRight];
 // prettier-ignore
 const symbols = [
   "{", "&", "*", "(", "}",
@@ -72,7 +72,7 @@ const mouse = [
   "", "⇦", "⇧", "⇩", "⇨",
   "R", "L", "M"
 ];
-const layer2 = [...symbols, ...mouse]
+const layer2 = [...symbols, ...mouse];
 // prettier-ignore
 const functions = [
   "F12", "F7", "F8", "F9", "PrtScr",
@@ -87,7 +87,7 @@ const media = [
   "◯", "1", "2", "3", "4",
   "Stop", "Play", "Mut"
 ];
-const layer3 = [...functions, ...media]
+const layer3 = [...functions, ...media];
 // prettier-ignore
 const nums = [
   "[", "7", "8", "9", "]",
@@ -101,21 +101,18 @@ const navigation = [
   "CapL", "⯇", "▲", "▼", "⯈",
   "Ins", "Home", "PgD", "PgU", "End",
   "↵", "⌫", "⌦"
-
-// ⯇ ⯈ ⯅ ⯆
-// ◀ ▶ ▲ ▼
 ];
-const layer4 = [...nums, ...navigation]
+const layer4 = [...nums, ...navigation];
 
 interface ButtonProps {
-  side: 'left' | 'right'
-  leftTop: string
-  rightTop: string
-  leftBottom: string
-  rightBottom: string
-  top: number
-  left: number
-  rotation: number
+  side: "left" | "right";
+  leftTop: string;
+  rightTop: string;
+  leftBottom: string;
+  rightBottom: string;
+  top: number;
+  left: number;
+  rotation: number;
 }
 
 const Button = (props: ButtonProps) => {
@@ -127,30 +124,31 @@ const Button = (props: ButtonProps) => {
     rightBottom,
     top,
     left,
-    rotation,
-  } = props
+    rotation
+  } = props;
 
   const colors =
-    side === 'left'
+    side === "left"
       ? {
-          leftTop: 'text-[#000000]',
-          rightTop: 'text-[#5cfc45]',
-          leftBottom: 'text-[#fd4526]',
-          rightBottom: 'text-[#4322fd]',
-        }
+        leftTop: "text-[#000000]",
+        rightTop: "text-[#5cfc45]",
+        leftBottom: "text-[#fd4526]",
+        rightBottom: "text-[#4322fd]"
+      }
       : {
-          leftTop: 'text-[#000000]',
-          rightTop: 'text-[#ffff1b]',
-          leftBottom: 'text-[#ff4fff]',
-          rightBottom: 'text-[#4bffff]',
-        }
+        leftTop: "text-[#000000]",
+        rightTop: "text-[#ffff1b]",
+        leftBottom: "text-[#ff4fff]",
+        rightBottom: "text-[#4bffff]"
+      };
 
   return (
     <div
-      className='absolute h-[54px] w-[54px] rounded-[5px] border border-black bg-[#888888] font-bold'
+      className="absolute h-[54px] w-[54px] rounded-[5px] border border-black bg-[#888888] font-bold"
       style={{ top, left, transform: `rotate(${rotation}deg)` }}
     >
-      <div className='absolute left-[5px] top-[3px] h-[42px] w-[42px] rounded-[3px] border border-[rgba(0,0,0,0.3)] bg-[#a7a7a7]'>
+      <div
+        className="absolute left-[5px] top-[3px] h-[42px] w-[42px] rounded-[3px] border border-[rgba(0,0,0,0.3)] bg-[#a7a7a7]">
         <div
           className={`absolute left-[1px] top-[1px] text-[0.75rem] ${colors.leftTop}`}
         >
@@ -173,33 +171,35 @@ const Button = (props: ButtonProps) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
-  const indicies = [...Array(36).keys()]
+  const indicies = [...Array(36).keys()];
 
   return (
-    <div className='App'>
-      <h1 className='text-3xl font-bold underline'>Miryoku Visualizer</h1>
-      <div className='h-4' />
-      <div className='relative'>
-        {indicies.map((i) => (
-          <Button
-            key={i}
-            top={yPos[i]}
-            left={xPos[i]}
-            rotation={rotation[i]}
-            side={i < 18 ? 'left' : 'right'}
-            leftTop={alphas[i]}
-            rightTop={layer2[i]}
-            leftBottom={layer3[i]}
-            rightBottom={layer4[i]}
-          />
-        ))}
+    <div className="App">
+      <div className="m-auto max-w-[650px] text-center">
+        <h1 className="text-3xl font-bold underline">Miryoku Visualizer</h1>
+        <div className="h-4" />
+        <div className="relative">
+          {indicies.map((i) => (
+            <Button
+              key={i}
+              top={yPos[i]}
+              left={xPos[i]}
+              rotation={rotation[i]}
+              side={i < 18 ? "left" : "right"}
+              leftTop={alphas[i]}
+              rightTop={layer2[i]}
+              leftBottom={layer3[i]}
+              rightBottom={layer4[i]}
+            />
+          ))}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
